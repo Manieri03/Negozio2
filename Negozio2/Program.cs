732b0b5b0a792a395b5f38e8c3f3f2a896c84c64
ITT Rosselli Rasetti <rosselli.rasetti@outlook.com>
2019-10-16 13:07:56 +0200

    Add project files.

---
+++ Negozio2/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Negozio2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double sconto=0;
+            double prezzoscontato=0;
+            double spesa2;
+            Console.WriteLine("Quanto hai speso?");
+            double spesa = double.Parse(Console.ReadLine());
+            if (spesa <= 300)
+            {
+                sconto = (spesa / 100) * 10;
+                prezzoscontato = spesa - sconto;
+                
+
+            }
+            else if (spesa > 300)
+            {
+                spesa2 = spesa - 300;
+                sconto = (spesa2 / 100) * 20 + 30;
+                prezzoscontato = spesa - sconto;
+                
+            }
+            Console.WriteLine($"La spesa è di {prezzoscontato}, dato lo sconto di {sconto}");
+            Console.ReadLine();
+
+        }
+    }
+}
